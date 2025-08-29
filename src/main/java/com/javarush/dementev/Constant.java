c9a28514a207b91ea4c582364761b4bbb3cf2d2f
”DragonIT” <info@dragonit.ru>
2025-08-29 08:44:36 +0300

    FIX Alphabet

--- src/main/java/com/javarush/dementev/Constant.java
+++ src/main/java/com/javarush/dementev/Constant.java
@@ -7,7 +7,7 @@ public class Constant {
     private static final String rus = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ";
     private static final String eng = "QWERTYUIOPASDFGHJKLZXCVBNM";
     private static final String numbers = "1234567890";
-    private static final String znaki = "!@#$%^&*()_+-=[];',./\\{}";
+    private static final String znaki = "!@#$%^&*()_+-=[];',./\\{} \"?";
     private final static String ALPHABET = rus + rus.toLowerCase() + eng + eng.toLowerCase() + numbers + znaki;
     public static final Map CHARSMAP = new HashMap();
     public static final char[] INDEXALPHABET = ALPHABET.toCharArray();
